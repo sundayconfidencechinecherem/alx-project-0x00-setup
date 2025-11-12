f9142a907675a546e8eefb16e332116c19c93106
sundayconfidencechinecherem <sundayconfidencechinecherem@gmail.com>
2025-11-12 10:57:19 +0100

    component Rendering

---
+++ alx-project-0x00/components/Pill.tsx
@@ -0,0 +1,11 @@
+
+
+const Pill: React.FC <{title: string}> =  ({title}) => {
+  return (
+    <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
+      <p className=" text-sm ">{title}</p>
+    </div>
+  )
+}
+
+export default Pill;
